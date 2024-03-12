7f42448536e5dcc64765dc0a1cb8795b4be0d607
jansari <jansari@entiovi.com>
2024-03-12 13:16:18 +0530

    shadcn-ui add to the project and we have tested the functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
 
 
 export default function Home() {
   return (
-   <h1 className="bg-gray-50 underline">Happy coding 2k24 Discord Clone </h1>
-  );
+<>
+<h1 className="bg-gray-50 underline">Happy coding 2k24 Discord Clone </h1>
+<Button variant="ghost" className="bg-red-400">Click me</Button>
+
+</>
+    );
 }
