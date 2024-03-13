acf9d99f1b5f5df3b69def1e51af69396fba949b
jansari <jansari@entiovi.com>
2024-03-14 00:54:47 +0530

    form modal created

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,3 +1,4 @@
+import { InitialModal } from "@/components/modals/initial-modal";
 import { db } from "@/lib/db";
 import { initialProfile } from "@/lib/initial-profile";
 import { redirect } from "next/navigation";
@@ -11,7 +12,6 @@ lets assume this profile is the member of the server
 and load it immediately for user for channel(general)
 
 */
-
   const server = await db.server.findFirst({
     where: {
       members: {
@@ -26,7 +26,7 @@ and load it immediately for user for channel(general)
    return redirect(`/server/${server.id}`);
   }
   
-  return <div>Create a Server</div>;
+  return <InitialModal/>;
 };
 
 export default SetupPage;
---
+++ components/modals/initial-modal.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import {
+  Dialog,
+  DialogContent,
+  DialogClose,
+  DialogFooter,
+  DialogTitle,
+  DialogHeader,
+  DialogDescription,
+} from "@/components/ui/dialog";
+
+export const InitialModal = () => {
+  const form = useForm({
+    defaultValues: {
+      name: "",
+      imageUrl: "",
+    },
+  });
+  return (
+    <Dialog open>
+      <DialogContent className="bg-white text-black p-0 overflow-hidden">
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl text-center font-bold">
+            Customize your server
+          </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            Give your server a personality with a name and an image. You can
+            always change it later.
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+};
