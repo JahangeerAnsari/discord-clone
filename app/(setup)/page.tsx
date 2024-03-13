d9e7c06bf42054eea9128a01fa441098b2eb4b22
jansari <jansari@entiovi.com>
2024-03-13 22:51:39 +0530

    user profile detail created and it will be unique if there is no any server for related profile then only we can create

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,32 @@
+import { db } from "@/lib/db";
+import { initialProfile } from "@/lib/initial-profile";
+import { redirect } from "next/navigation";
+
+const SetupPage = async () => {
+  const profile = await initialProfile();
+/*
+we gonna find out profileId one of the members 
+in the server
+lets assume this profile is the member of the server
+and load it immediately for user for channel(general)
+
+*/
+
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if(server){
+   return redirect(`/server/${server.id}`);
+  }
+  
+  return <div>Create a Server</div>;
+};
+
+export default SetupPage;
