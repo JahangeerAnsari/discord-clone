26ce81eb3dfb0f6f2c326e7d073c6917e994cb4a
jansari <jansari@entiovi.com>
2024-03-14 01:52:04 +0530

    zod form validation completed with react-hook-form

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import {
   Dialog,
   DialogContent,
@@ -10,14 +12,49 @@ import {
   DialogHeader,
   DialogDescription,
 } from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
+// form schema
+const formSchema = z.object({
+  name: z.string().min(1, {
+    message: "Server name is required!",
+  }),
+  imageUrl: z.string().min(1, {
+    message: "Server Image is required!",
+  }),
+});
 
 export const InitialModal = () => {
+ const [isMounted, setIsMounted] = useState(false)
+
+ useEffect(() =>{
+setIsMounted(true);
+ },[])
   const form = useForm({
+    resolver: zodResolver(formSchema),
     defaultValues: {
       name: "",
       imageUrl: "",
     },
   });
+  // extract loading state from form
+  const isLoading = form.formState.isSubmitting;
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log("values", values);
+  };
+
+  if(!isMounted){
+   return null;
+  }
   return (
     <Dialog open>
       <DialogContent className="bg-white text-black p-0 overflow-hidden">
@@ -25,11 +62,54 @@ export const InitialModal = () => {
           <DialogTitle className="text-2xl text-center font-bold">
             Customize your server
           </DialogTitle>
-          <DialogDescription className="text-center text-zinc-500">
+          <DialogDescription className="text-center text-zinc-500 font-bold">
             Give your server a personality with a name and an image. You can
             always change it later.
           </DialogDescription>
         </DialogHeader>
+        <Form {...form}>
+          {/* going to use handleForm  from {...form} 
+         and we have also pass by the values onSubmit
+         */}
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <div className="space-y-8 px-6">
+              <div className="flex items-center justify-center text-center">
+                TODO: Image Upload
+              </div>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel
+                      className="uppercase text-xs font-bold
+                     text-zinc-500 dark:text-secondary/70"
+                    >
+                      Server Name
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={isLoading}
+                        className="bg-zinc-300/50 border-0
+                     focus-visible:ring-0 text-black 
+                     focus-visible:ring-offset-0
+                     "
+                        placeholder="Enter server name"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage/>
+                  </FormItem>
+                )}
+              />
+            </div>
+            <DialogFooter className="bg-gray-100 px-6 py-4">
+              <Button variant="primary" disabled={isLoading}>
+               Create
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
