5ada3a28053542fd8c8785bb71e97b082340ef6e
jansari <jansari@entiovi.com>
2024-03-24 22:01:44 +0530

    update channel from channel list implemented

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -2,7 +2,7 @@ import {create} from 'zustand';
 import { Channel, ChannelType, Server } from '@prisma/client';
 export type ModalType = 'createServer' | 'invite' | "editServer"  | 'members' 
 | "createChannel" | "leaveServer" | "deleteServer"
-|"deleteChannel" 
+|"deleteChannel" | "editChannel"
 ; 
 interface ModalData{
  server?:Server,
