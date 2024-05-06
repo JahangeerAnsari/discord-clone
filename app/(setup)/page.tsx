1422986d82e65046ab0eeff3003c05eac8e82bcf
jansari <jansari@entiovi.com>
2024-05-06 17:29:50 +0530

    prisma initialization and model created for this

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,29 @@
+import { db } from "@/lib/db";
+import { initialProfile } from "@/lib/initial-profile";
+import { redirect } from "next/navigation";
+
+ const SetupPage = async () => {
+  const profile = await initialProfile();
+  // lets find the profile is the member of server 
+  // if there is then immediately load the server for profile
+  const server = await db.server.findFirst({
+   where:{
+    members:{
+     some:{
+      profileId: profile.id,
+     }
+    }
+   }
+  })
+  if(server){
+   // the immediately redirect to the server for
+   return redirect(`/servers/${server.id}`)
+  }
+  return (  
+   <div>
+    Create a Server
+   </div>
+  );
+ }
+  
+ export default SetupPage;
